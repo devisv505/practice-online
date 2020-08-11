b4dd19d3df360de1eb9f7bea4b30311748b8be7c
devisv505 <devisv505@gmail.com>
2020-08-11 15:22:44 +0300

    first commit

---
+++ src/main/java/com/devisv/practice/online/PracticeOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.devisv.practice.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeOnlineApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PracticeOnlineApplication.class, args);
+  }
+
+}
